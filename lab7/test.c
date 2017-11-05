a5c266c65d9d52fd82abd8275b13762ca34c03b7
changyan <327643958@qq.com>
2017-11-05 21:14:15 +0800

    test

---
+++ lab7/test.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include "menu.h"
+
+void Quit(int argc, char *argv[]);
+void Version(int argc, char *argv[]);
+
+
+void Quit(int argc, char *argv[])
+{
+    exit(0);
+}
+
+void Version(int argc, char *argv[])
+{
+    printf("version1.0\n");
+}
+
+
+int main(int argc,char* argv[])
+{
+    MenuConfig("version","XXX1.0(Menu program v1.0)",NULL);
+    MenuConfig("quit","Quit",Quit);
+    
+
+    ExecuteMenu();
+}
